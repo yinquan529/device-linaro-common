2f2c13707a87824e996e7f0531063f4cef3151e9
Patrik Ryd <patrik.ryd@linaro.org>
2011-04-07 18:14:50 +0200

    Use TARGET_TOOLS_PREFIX when building kernel

--- tasks/kernel.mk
+++ tasks/kernel.mk
@@ -1,7 +1,7 @@
 android_kernel:
 	cd $(TOP)/kernel &&\
-	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- defconfig $(KERNEL_CONFIG) &&\
-	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage
+	make ARCH=arm CROSS_COMPILE=../$(TARGET_TOOLS_PREFIX) defconfig $(KERNEL_CONFIG) &&\
+	make ARCH=arm CROSS_COMPILE=../$(TARGET_TOOLS_PREFIX) uImage
 
 $(PRODUCT_OUT)/kernel: android_kernel
 	echo HUPP : $(PRODUCT_OUT) : $(TOP) : $(KERNEL_CONFIG) : $(PRODUCT_OUT)/kernel
