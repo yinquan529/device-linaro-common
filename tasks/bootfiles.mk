1f810178360cd6420366f49d6191b8fc139f0ba8
Patrik Ryd <patrik.ryd@linaro.org>
2011-04-11 17:11:02 +0200

    Added support for xloader (MLO)

--- tasks/bootfiles.mk
+++ tasks/bootfiles.mk
@@ -8,5 +8,8 @@ bootfiles:
 ifeq ($(TARGET_USE_UBOOT),true)
 	cp $(PRODUCT_OUT)/u-boot.bin $(PRODUCT_OUT)/boot
 endif
+ifeq ($(TARGET_USE_XLOADER),true)
+	cp $(XLOADER_BINARY) $(PRODUCT_OUT)/boot
+endif
 
 $(INSTALLED_BOOTTARBALL_TARGET): bootfiles
