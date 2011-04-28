cc108a6396661b453085fd4be7011f69acf52d87
Alexander Sack <asac@linaro.org>
2011-04-28 12:17:49 +0200

    Support absolute path to toolchain when building kernel
    Signed-off-by: Patrik Ryd <patrik.ryd@linaro.org>

--- tasks/kernel.mk
+++ tasks/kernel.mk
@@ -1,7 +1,7 @@
 android_kernel:
 	cd $(TOP)/kernel &&\
-	make ARCH=arm CROSS_COMPILE=../$(TARGET_TOOLS_PREFIX) defconfig $(KERNEL_CONFIG) &&\
-	make ARCH=arm CROSS_COMPILE=../$(TARGET_TOOLS_PREFIX) uImage
+	make ARCH=arm CROSS_COMPILE=$(shell sh -c "cd $(TOP); cd `dirname $(TARGET_TOOLS_PREFIX)`; pwd")/$(shell basename $(TARGET_TOOLS_PREFIX)) defconfig $(KERNEL_CONFIG) &&\
+	make ARCH=arm CROSS_COMPILE=$(shell sh -c "cd $(TOP); cd `dirname $(TARGET_TOOLS_PREFIX)`; pwd")/$(shell basename $(TARGET_TOOLS_PREFIX)) uImage
 
 $(PRODUCT_OUT)/uImage: android_kernel
 	ln -sf ../../../../kernel/arch/arm/boot/uImage $(PRODUCT_OUT)/uImage
