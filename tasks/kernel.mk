1f29ec89c6bb1b8f1f5ac320c2448ba70c0cff91
Zach Pfeffer <zach.pfeffer@linaro.org>
2011-07-26 16:19:00 -0500

    kernel.mk Change module install location to /system/modules

--- tasks/kernel.mk
+++ tasks/kernel.mk
@@ -6,8 +6,8 @@ android_kernel:
 	mkdir -p $(TOP)/kernel/modules_for_android
 	cd $(TOP)/kernel &&\
 	make ARCH=arm CROSS_COMPILE=$(shell sh -c "cd $(TOP); cd `dirname $(TARGET_TOOLS_PREFIX)`; pwd")/$(shell basename $(TARGET_TOOLS_PREFIX)) modules_install INSTALL_MOD_PATH=modules_for_android
-	mkdir -p $(TARGET_OUT)/lib/modules
-	find kernel/modules_for_android -name "*.ko" -exec $(ACP) -fpt {} $(TARGET_OUT)/lib/modules/ \;
+	mkdir -p $(TARGET_OUT)/modules
+	find kernel/modules_for_android -name "*.ko" -exec $(ACP) -fpt {} $(TARGET_OUT)/modules/ \;
 
 
 $(PRODUCT_OUT)/uImage: android_kernel
