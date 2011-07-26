dd6ac6e03010de8ad5d4589e9ea04d35a65cb567
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-07-26 21:46:33 +0159

    Build u-boot.imx on iMX53 targets

--- tasks/bootfiles.mk
+++ tasks/bootfiles.mk
@@ -7,6 +7,9 @@ bootfiles:
 	$(hide) mkdir -p $(PRODUCT_OUT)/boot
 ifeq ($(TARGET_USE_UBOOT),true)
 	cp $(PRODUCT_OUT)/u-boot.bin $(PRODUCT_OUT)/boot
+ifeq ($(TARGET_PRODUCT), iMX53)
+	cp -L $(PRODUCT_OUT)/u-boot.imx $(PRODUCT_OUT)/boot
+endif
 endif
 ifeq ($(TARGET_USE_XLOADER),true)
 	cp $(XLOADER_BINARY) $(PRODUCT_OUT)/boot
--- tasks/uboot.mk
+++ tasks/uboot.mk
@@ -3,7 +3,13 @@ android_uboot:
 	cd $(TOP)/u-boot &&\
 	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=arm-linux-gnueabi- $(UBOOT_CONFIG) &&\
 	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=arm-linux-gnueabi- 
+ifeq ($(TARGET_PRODUCT), iMX53)
+	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=arm-linux-gnueabi- ../$(PRODUCT_OUT)/obj/u-boot/u-boot.imx
+endif
 
 $(PRODUCT_OUT)/u-boot.bin: android_uboot
 	ln -sf obj/u-boot/u-boot.bin $(PRODUCT_OUT)/u-boot.bin
+ifeq ($(TARGET_PRODUCT), iMX53)
+	ln -sf obj/u-boot/u-boot.imx $(PRODUCT_OUT)/u-boot.imx
+endif
 
