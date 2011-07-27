b155af305d73fe247fafbe21b3c5a3d17ed80693
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-07-28 00:06:07 +0159

    u-boot makefiles don't seem to like building u-boot.imx out-of-source at all

--- tasks/uboot.mk
+++ tasks/uboot.mk
@@ -4,12 +4,14 @@ android_uboot:
 	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=arm-linux-gnueabi- $(UBOOT_CONFIG) &&\
 	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=arm-linux-gnueabi- 
 ifeq ($(TARGET_PRODUCT), iMX53)
-	make CROSS_COMPILE=arm-linux-gnueabi- $(UBOOT_CONFIG) && make CROSS_COMPILE=arm-linux-gnueabi- u-boot.imx
+	cd $(TOP)/u-boot &&\
+	make CROSS_COMPILE=arm-linux-gnueabi- $(UBOOT_CONFIG) && \
+	make CROSS_COMPILE=arm-linux-gnueabi- u-boot.imx
 endif
 
 $(PRODUCT_OUT)/u-boot.bin: android_uboot
 	ln -sf obj/u-boot/u-boot.bin $(PRODUCT_OUT)/u-boot.bin
 ifeq ($(TARGET_PRODUCT), iMX53)
-	cp u-boot.imx $(PRODUCT_OUT)/u-boot.imx
+	cp $(TOP)/u-boot/u-boot.imx $(PRODUCT_OUT)/u-boot.imx
 endif
 
