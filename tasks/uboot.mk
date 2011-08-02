c8d7c6ee6ae806cac4fc58fba320e1acfb564fe1
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-08-02 17:20:20 +0159

    - Don't hardcode the arm-linux-gnueabi- toolchain prefix - Use the correct toolchain specified by TARGET_TOOLS_PREFIX

--- tasks/uboot.mk
+++ tasks/uboot.mk
@@ -1,12 +1,17 @@
+TCDIR = $(shell dirname $(TARGET_TOOLS_PREFIX))
+TCPREFIX = $(shell basename $(TARGET_TOOLS_PREFIX))
+
 android_uboot:
 	mkdir -p $(PRODUCT_OUT)/obj/u-boot
 	cd $(TOP)/u-boot &&\
-	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=arm-linux-gnueabi- $(UBOOT_CONFIG) &&\
-	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=arm-linux-gnueabi- 
+	export PATH=$(TCDIR):$(PATH) && \
+	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=$(TCPREFIX) $(UBOOT_CONFIG) &&\
+	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=$(TCPREFIX)
 ifeq ($(TARGET_PRODUCT), iMX53)
 	cd $(TOP)/u-boot &&\
-	make CROSS_COMPILE=arm-linux-gnueabi- $(UBOOT_CONFIG) && \
-	make CROSS_COMPILE=arm-linux-gnueabi- u-boot.imx
+	export PATH=$(TCDIR):$(PATH) && \
+	make CROSS_COMPILE=$(TCPREFIX) $(UBOOT_CONFIG) && \
+	make CROSS_COMPILE=$(TCPREFIX) u-boot.imx
 endif
 
 $(PRODUCT_OUT)/u-boot.bin: android_uboot
