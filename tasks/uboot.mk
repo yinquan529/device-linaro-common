3219f8aff287b2d278c016beb0633894515dfb38
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-08-29 10:44:42 +0159

    Use the same toolchain we're using for the rest of the build
    This was (apparently) not done before because the old
    version of binutils included in toolchains before 11.08
    fails to link uboot.
    
    With the 11.08 toolchain, we should be ok.

--- tasks/uboot.mk
+++ tasks/uboot.mk
@@ -1,23 +1,17 @@
-TCPREFIX = arm-linux-gnueabi-
-# The 2 lines below (instead of the one above) are actually the right
-# thing to do -- grabbing the toolchain we're meant to use.
-# Unfortunately the current (4.6-2011.07-0-8-2011-07-25_12-42-06-linux-x86)
-# ld fails to link u-boot, so we revert to the old behavior (since it
-# picks up an older, working ld on the build machines) for now.
-#TCDIR = $(shell dirname $(TARGET_TOOLS_PREFIX))
-#TCPREFIX = $(shell basename $(TARGET_TOOLS_PREFIX))
+UBOOT_TCDIR := $(shell dirname $(TARGET_TOOLS_PREFIX))
+UBOOT_TCPREFIX := $(shell basename $(TARGET_TOOLS_PREFIX))
 
 android_uboot:
 	mkdir -p $(PRODUCT_OUT)/obj/u-boot
 	cd $(TOP)/u-boot &&\
-	export PATH=$(TCDIR):$(PATH) && \
-	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=$(TCPREFIX) $(UBOOT_CONFIG) &&\
-	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=$(TCPREFIX)
+	export PATH=$(UBOOT_TCDIR):$(PATH) && \
+	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=$(UBOOT_TCPREFIX) $(UBOOT_CONFIG) &&\
+	make O=../$(PRODUCT_OUT)/obj/u-boot CROSS_COMPILE=$(UBOOT_TCPREFIX)
 ifeq ($(TARGET_PRODUCT), iMX53)
 	cd $(TOP)/u-boot &&\
-	export PATH=$(TCDIR):$(PATH) && \
-	make CROSS_COMPILE=$(TCPREFIX) $(UBOOT_CONFIG) && \
-	make CROSS_COMPILE=$(TCPREFIX) u-boot.imx
+	export PATH=$(UBOOT_TCDIR):$(PATH) && \
+	make CROSS_COMPILE=$(UBOOT_TCPREFIX) $(UBOOT_CONFIG) && \
+	make CROSS_COMPILE=$(UBOOT_TCPREFIX) u-boot.imx
 endif
 
 $(PRODUCT_OUT)/u-boot.bin: android_uboot
