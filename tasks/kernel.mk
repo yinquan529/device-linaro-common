58cc590dc38f8e796c12b89a48110186a2f2eb9c
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-12-06 13:55:56 +0059

    Revert "tasks: switch to the host toolchain for kernel and u-boot"
    This reverts commit fea8832c3915ef0dbf2e598b407638d68245e677.

--- tasks/kernel.mk
+++ tasks/kernel.mk
@@ -4,15 +4,18 @@ endif
 
 android_kernel: $(PRODUCT_OUT)/u-boot.bin
 	cd $(TOP)/kernel &&\
-	$(MAKE) -j1 $(KERNEL_VERBOSE) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- defconfig $(KERNEL_CONFIG) &&\
-	$(MAKE) $(KERNEL_VERBOSE) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage
+	if [ -e $(TARGET_TOOLS_PREFIX)ld.bfd ]; then LD=$(TARGET_TOOLS_PREFIX)ld.bfd; else LD=$(TARGET_TOOLS_PREFIX)ld; fi && \
+	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
+	$(MAKE) -j1 $(KERNEL_VERBOSE) ARCH=arm CROSS_COMPILE=$(shell sh -c "cd $(TOP); cd `dirname $(TARGET_TOOLS_PREFIX)`; pwd")/$(shell basename $(TARGET_TOOLS_PREFIX)) LD=$$LD defconfig $(KERNEL_CONFIG) &&\
+	$(MAKE) $(KERNEL_VERBOSE) ARCH=arm CROSS_COMPILE=$(shell sh -c "cd $(TOP); cd `dirname $(TARGET_TOOLS_PREFIX)`; pwd")/$(shell basename $(TARGET_TOOLS_PREFIX)) LD=$$LD uImage
 
 android_kernel_modules: $(INSTALLED_KERNEL_TARGET) $(ACP)
 	cd $(TOP)/kernel &&\
-	$(MAKE) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules
+	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
+	$(MAKE) ARCH=arm CROSS_COMPILE=$(shell sh -c "cd $(TOP); cd `dirname $(TARGET_TOOLS_PREFIX)`; pwd")/$(shell basename $(TARGET_TOOLS_PREFIX)) modules
 	mkdir -p $(TOP)/kernel/modules_for_android
 	cd $(TOP)/kernel &&\
-	$(MAKE) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules_install INSTALL_MOD_PATH=modules_for_android
+	$(MAKE) ARCH=arm CROSS_COMPILE=$(shell sh -c "cd $(TOP); cd `dirname $(TARGET_TOOLS_PREFIX)`; pwd")/$(shell basename $(TARGET_TOOLS_PREFIX)) modules_install INSTALL_MOD_PATH=modules_for_android
 	mkdir -p $(TARGET_OUT)/modules
 	find kernel/modules_for_android -name "*.ko" -exec $(ACP) -fpt {} $(TARGET_OUT)/modules/ \;
 
@@ -21,7 +24,7 @@ ifeq ($(TARGET_USE_GATOR),true)
 KERNEL_PATH:=$(shell pwd)/kernel
 gator_driver: android_kernel_modules $(INSTALLED_KERNEL_TARGET) $(ACP)
 	cd $(TOP)/external/gator/driver &&\
-	$(MAKE) ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -C $(KERNEL_PATH) M=`pwd` modules
+	$(MAKE) ARCH=arm CROSS_COMPILE=$(shell sh -c "cd $(TOP); cd `dirname $(TARGET_TOOLS_PREFIX)`; pwd")/$(shell basename $(TARGET_TOOLS_PREFIX)) -C $(KERNEL_PATH) M=`pwd` modules
 	mkdir -p $(TARGET_OUT)/modules
 	find $(TOP)/external/gator/driver/. -name "*.ko" -exec $(ACP) -fpt {} $(TARGET_OUT)/modules/ \;
 else
