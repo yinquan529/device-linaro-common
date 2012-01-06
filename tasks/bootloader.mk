067c1d9165003c8e209ce2d9ad38e2475554e31e
Jon Medhurst <tixy@linaro.org>
2012-01-06 14:14:51 +0000

    tasks/bootloader.mk: Add support for additional/custom bootloaders
    This adds a mechanism for building additional versions of U-Boot by
    defining their configs and names in the variable UBOOT_FLAVOURS. This
    will be used by Vexpress to provide a single Android image which will
    work with A5, A9 and A15 CPUs.
    
    This change also provides a hook for boards to provide customised rules
    for building bootloaders (CUSTOM_BOOTLOADER_MAKEFILE).
    
    It is envisaged that this new task makefile and hooks will be used to
    add support for using a UEFI bootloader and a means of tidying up the
    way U-Boot is built in uboot.mk and bootfiles.mk.
    
    Change-Id: I8bdddab60e429a8bdf4f022b752b524a980afc5e
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

---
+++ tasks/bootloader.mk
@@ -0,0 +1,50 @@
+# Rules for building bootloaders
+
+BOOTLOADER_TARGETS :=
+
+#
+# Generate a rule to build U-Boot.
+#
+# Usage: $(eval $(call MAKE_UBOOT, target-binary, config-name))
+#
+#   target-binary   Path and name for generated u-boot.bin
+#   config-name     Without trailing '_config'
+#
+#
+define MAKE_UBOOT
+
+$(1): $$(ACP)
+	$$(eval _obj := $$(PRODUCT_OUT)/obj/u-boot.$(2))
+	@mkdir -p $$(_obj)
+	cd $$(TOP)/u-boot && \
+	if [ -e $$(UBOOT_TCDIR)/$$(UBOOT_TCPREFIX)ld.bfd ]; then ln -sf $$(UBOOT_TCDIR)/$$(UBOOT_TCPREFIX)ld.bfd $$(UBOOT_TCPREFIX)ld; fi && \
+	export PATH=`pwd`:$$(UBOOT_TCDIR):$$(PATH) && \
+	$$(MAKE) O=../$$(_obj) CROSS_COMPILE=$$(UBOOT_TCPREFIX) $(2)_config && \
+	$$(MAKE) O=../$$(_obj) CROSS_COMPILE=$$(UBOOT_TCPREFIX)
+	@mkdir -p $$(dir $$@)
+	$$(ACP) -fpt $$(_obj)/u-boot.bin $$@
+
+endef
+
+#
+# UBOOT_FLAVOURS contains a list of extra U-Boot binaries to build, each
+# entry in the list is in the form <config-name>:<binary-name>
+#
+UBOOT_FLAVOUR_TARGETS :=
+$(foreach _ub,$(UBOOT_FLAVOURS),                      \
+    $(eval _config := $(call word-colon,1,$(_ub)))    \
+    $(eval _binary := $(call word-colon,2,$(_ub)))    \
+    $(eval _target := $(PRODUCT_OUT)/boot/$(_binary)) \
+    $(eval $(call MAKE_UBOOT,$(_target),$(_config)))  \
+    $(eval UBOOT_FLAVOUR_TARGETS += $(_target))       \
+    )
+
+BOOTLOADER_TARGETS += $(UBOOT_FLAVOUR_TARGETS)
+
+
+ifneq ($(CUSTOM_BOOTLOADER_MAKEFILE),)
+include $(TOP)/$(CUSTOM_BOOTLOADER_MAKEFILE)
+endif
+
+
+$(INSTALLED_BOOTTARBALL_TARGET): $(BOOTLOADER_TARGETS)
