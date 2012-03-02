aebc23f0aeb01d687b2c87632e59d24814dd607a
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-03-02 13:58:46 +0100

    tasks/kernel.mk: Remove iMX6 DeviceTree block
    iMX6 now uses DEVICE_TREES in its BoardConfig.mk, no need
    to have to custom DeviceTree generation code in here anymore.
    
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- tasks/kernel.mk
+++ tasks/kernel.mk
@@ -22,15 +22,6 @@ android_kernel: $(PRODUCT_OUT)/u-boot.bin
 	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
 	$(MAKE) -j1 KCFLAGS="$(TARGET_EXTRA_CFLAGS) -fno-pic $(LOCAL_CFLAGS)" $(KERNEL_VERBOSE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) LD=$$LD defconfig $(KERNEL_CONFIG) &&\
 	$(MAKE) $(KERNEL_VERBOSE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) KCFLAGS="$(TARGET_EXTRA_CFLAGS) -fno-pic $(LOCAL_CFLAGS)" LD=$$LD uImage
-ifeq ($(TARGET_PRODUCT), iMX6)
-	# DeviceTree bits...
-	cd $(TOP)/kernel &&\
-	if [ -e $(KERNEL_TOOLS_PREFIX)ld.bfd ]; then LD=$(KERNEL_TOOLS_PREFIX)ld.bfd; else LD=$(KERNEL_TOOLS_PREFIX)ld; fi && \
-	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
-	$(MAKE) $(KERNEL_VERBOSE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) KCFLAGS="$(TARGET_EXTRA_CFLAGS) -fno-pic $(LOCAL_CFLAGS)" LD=$$LD imx6q-sabrelite.dtb && \
-	mkdir -p $(PRODUCT_OUT)/boot
-	cp $(PRODUCT_OUT)/obj/kernel/arch/arm/boot/imx6q-sabrelite.dtb $(PRODUCT_OUT)/boot/board.dtb
-endif
 
 android_kernel_modules: $(INSTALLED_KERNEL_TARGET) $(ACP)
 	cd $(TOP)/kernel &&\
