76bbb8c198ac69e503c0e5e866ae4afbd097bb69
Zach Pfeffer <zach.pfeffer@linaro.org>
2012-10-16 20:41:19 -0500

    kernel.mk: Fix multiple PATH extensions
    Change-Id: I95bb58f517b8a965ff3ef67f8593b897a80ea525
    Signed-off-by: Zach Pfeffer <zach.pfeffer@linaro.org>

--- tasks/kernel.mk
+++ tasks/kernel.mk
@@ -32,41 +32,37 @@ endif
 ifeq ($(strip $(ANDROID_64)),true)
 ARCH := arm64
 LOCAL_CFLAGS=
+KERNEL_COMPILER_PATHS := $(REALTOP)/gcc-linaro-aarch64-linux-gnu-4.7/bin:../$(BUILD_OUT_EXECUTABLES)
 else
 ARCH := arm
 LOCAL_CFLAGS=$(call cc-option,"-mno-unaligned-access", )
+KERNEL_COMPILER_PATHS := ../$(BUILD_OUT_EXECUTABLES)
 endif
 
 android_kernel: $(BOOTLOADER_DEP) $(PERF_DEP)
-ifeq ($(strip $(ANDROID_64)),true)
-	export PATH=$(REALTOP)/gcc-linaro-aarch64-linux-gnu-4.7/bin:$(PATH)
-endif
 	echo building kernel $(KERNEL_TARGET) with config $(KERNEL_CONFIG) for bootloader $(TARGET_BOOTLOADER_TYPE)
 	mkdir -p $(KERNEL_OUT)
 	cd $(TOP)/kernel &&\
+	export PATH=$(KERNEL_COMPILER_PATHS):$(PATH) && \
 	if [ -e $(KERNEL_TOOLS_PREFIX)ld.bfd ]; then LD=$(KERNEL_TOOLS_PREFIX)ld.bfd; else LD=$(KERNEL_TOOLS_PREFIX)ld; fi && \
-	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
 	if [ $(words $(KERNEL_CONFIG)) -gt 1 ]; \
 	then scripts/kconfig/merge_config.sh -m $(KERNEL_CONFIG) && mv -f .config $(KERNEL_OUT)/.merged.config && $(MAKE) -j1 $(KERNEL_VERBOSE) O=$(KERNEL_OUT) ARCH=$(ARCH) KCONFIG_ALLCONFIG=$(KERNEL_OUT)/.merged.config alldefconfig; \
 	else $(MAKE) -j1 KCFLAGS="$(TARGET_EXTRA_CFLAGS) -fno-pic $(LOCAL_CFLAGS)" $(KERNEL_VERBOSE) O=$(KERNEL_OUT) ARCH=$(ARCH) CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) LD=$$LD defconfig $(KERNEL_CONFIG); \
 	fi && \
 	$(MAKE) $(KERNEL_VERBOSE) O=$(KERNEL_OUT) ARCH=$(ARCH) CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) KCFLAGS="$(TARGET_EXTRA_CFLAGS) -fno-pic $(LOCAL_CFLAGS)" LD=$$LD $(KERNEL_TARGET)
 ifeq ($(INCLUDE_PERF),1)
+	export PATH=$(KERNEL_COMPILER_PATHS):$(PATH) &&\
 	cd $(TOP)/kernel/tools/perf &&\
 	mkdir -p $(KERNEL_OUT)/tools/perf &&\
 	if [ -e $(KERNEL_TOOLS_PREFIX)ld.bfd ]; then LD=$(KERNEL_TOOLS_PREFIX)ld.bfd; else LD=$(KERNEL_TOOLS_PREFIX)ld; fi && \
-	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
 	$(MAKE) EXTRA_CFLAGS="$(TARGET_EXTRA_CFLAGS) $(LOCAL_CFLAGS) -isystem $(REALTOP)/bionic/libc/include -isystem $(REALTOP)/bionic/libc/kernel/common -isystem $(REALTOP)/bionic/libc/kernel/arch-arm -isystem $(REALTOP)/bionic/libc/arch-arm/include -I$(REALTOP)/external/elfutils/libelf -isystem $(REALTOP)/bionic/libm/include -isystem $(shell dirname $(KERNEL_TOOLS_PREFIX))/../include -I$(KERNEL_OUT)/tools/perf" BASIC_LDFLAGS="-nostdlib -Wl,-dynamic-linker,/system/bin/linker,-z,muldefs$(shell if test $(PLATFORM_SDK_VERSION) -lt 16; then echo -ne ',-T$(REALTOP)/$(BUILD_SYSTEM)/armelf.x'; fi),-z,nocopyreloc,--no-undefined -L$(REALTOP)/$(TARGET_OUT_STATIC_LIBRARIES) -L$(REALTOP)/$(PRODUCT_OUT)/system/lib -L$(REALTOP)/external/elfutils -L$(realpath $(PRODUCT_OUT))/obj/STATIC_LIBRARIES/libelf_intermediates -lpthread -lelf -lm -lc $(REALTOP)/$(TARGET_CRTBEGIN_DYNAMIC_O) $(REALTOP)/$(TARGET_CRTEND_O)" $(KERNEL_VERBOSE) O=$(KERNEL_OUT)/tools/perf/ OUTPUT=$(KERNEL_OUT)/tools/perf/ ARCH=$(ARCH) CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) LD=$$LD prefix=/system NO_DWARF=1 NO_NEWT=1 NO_LIBPERL=1 NO_LIBPYTHON=1 NO_GTK2=1 NO_STRLCPY=1 WERROR=0 && \
 	cp -f $(KERNEL_OUT)/tools/perf/perf $(REALTOP)/$(PRODUCT_OUT)/system/bin/
 endif
 
 android_kernel_modules: $(INSTALLED_KERNEL_TARGET) $(ACP)
-ifeq ($(strip $(ANDROID_64)),true)
-	export PATH=$(REALTOP)/gcc-linaro-aarch64-linux-gnu-4.7/bin:$(PATH)
-endif
+	export PATH=$(KERNEL_COMPILER_PATHS):$(PATH) &&\
 	cd $(TOP)/kernel &&\
 	if [ -e $(KERNEL_TOOLS_PREFIX)ld.bfd ]; then LD=$(KERNEL_TOOLS_PREFIX)ld.bfd; else LD=$(KERNEL_TOOLS_PREFIX)ld; fi && \
-	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
 	$(MAKE) $(KERNEL_VERBOSE) O=$(KERNEL_OUT) ARCH=$(ARCH) CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) LD=$$LD EXTRA_CFLAGS="$(EXTRA_CFLAGS) -fno-pic" KCFLAGS="$(TARGET_EXTRA_CFLAGS) -fno-pic $(LOCAL_CFLAGS)" modules
 	mkdir -p $(KERNEL_OUT)/modules_for_android
 	cd $(TOP)/kernel &&\
@@ -91,12 +87,9 @@ endif
 
 ifneq ($(realpath $(TOP)/external/gator/driver),)
 gator_driver: android_kernel_modules $(INSTALLED_KERNEL_TARGET) $(ACP)
-ifeq ($(strip $(ANDROID_64)),true)
-	export PATH=$(REALTOP)/gcc-linaro-aarch64-linux-gnu-4.7/bin:$(PATH)
-endif
+	export PATH=$(KERNEL_COMPILER_PATHS):$(PATH) &&\
 	cd $(TOP)/external/gator/driver &&\
 	if [ -e $(KERNEL_TOOLS_PREFIX)ld.bfd ]; then LD=$(KERNEL_TOOLS_PREFIX)ld.bfd; else LD=$(KERNEL_TOOLS_PREFIX)ld; fi && \
-	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
 	$(MAKE) O=$(KERNEL_OUT) ARCH=$(ARCH) CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) LD=$$LD EXTRA_CFLAGS="$(EXTRA_CFLAGS) -fno-pic $(GATOR_EXTRA_CFLAGS)" KCFLAGS="$(TARGET_EXTRA_CFLAGS) -fno-pic $(LOCAL_CFLAGS)" $(GATOR_EXTRA_MAKE_ARGS) -C $(KERNEL_PATH) M=`pwd` modules
 	mkdir -p $(TARGET_OUT)/modules
 	find $(TOP)/external/gator/driver/. -name "*.ko" -exec $(ACP) -fpt {} $(TARGET_OUT)/modules/ \;
@@ -155,11 +148,8 @@ ifneq ($(strip $(DTB_TARGETS)),)
 
 .PHONY : all_dtbs
 all_dtbs : $(INSTALLED_KERNEL_TARGET)
-ifeq ($(strip $(ANDROID_64)),true)
-	export PATH=$(REALTOP)/gcc-linaro-aarch64-linux-gnu-4.7/bin:$(PATH)
-endif
+	export PATH=$(KERNEL_COMPILER_PATHS):$(PATH) &&\
 	cd $(TOP)/kernel && \
-	export PATH=../$(BUILD_OUT_EXECUTABLES):$(PATH) && \
 	$(MAKE) O=$(KERNEL_OUT) ARCH=$(ARCH) CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) $(DTB_TARGETS)
 
 $(patsubst %,$(KERNEL_OUT)/arch/arm/boot/%,$(DTB_TARGETS)) : all_dtbs
