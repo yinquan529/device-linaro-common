e138a77acca8ce389c7b0a6c2fb2f79b87dbc676
Vishal Bhoj <vishal.bhoj@linaro.org>
2013-09-20 09:47:22 +0100

    common: Add linaro_configs.mk to store common build variables
    This needs to be inherited from Boardconfig.mk of the product.
    
    Change-Id: Ie9585e169cce123046b566326956ad291f757cb9
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

---
+++ linaro_configs.mk
@@ -0,0 +1,8 @@
+#
+# Use this file to set common Android build variables
+#
+
+#
+# Select 4.8 Android toolchain.
+#
+TARGET_TOOLS_PREFIX ?= prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8-linaro/bin/arm-linux-androideabi-
