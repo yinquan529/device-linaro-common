64e1fd03ac34100160ba57a1d752104a2a6d23e6
Amit Pundir <amit.pundir@linaro.org>
2013-10-07 14:22:38 +0530

    bootfiles.mk: copy FVP Base preboot binaries
    Change-Id: I83185384132d3fe355b4e7670bb31323d43aad0e
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- tasks/bootfiles.mk
+++ tasks/bootfiles.mk
@@ -67,6 +67,9 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM),arndale)
 	cp -r $(TOP)/device/linaro/arndale/arndale-bl1.bin $(PRODUCT_OUT)/boot
 endif
+ifeq ($(ANDROID_64),true)
+	cp -r $(TOP)/fvp-pre-boot/*.bin $(PRODUCT_OUT)/boot
+endif
 
 $(INSTALLED_BOOTTARBALL_TARGET): copybootfiles
 
