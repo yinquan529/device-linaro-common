3f00a3742fd188929e225a2c62c6cd832848d2e6
harigollamudi <harigopal.gollamudi@linaro.org>
2014-03-07 03:58:24 +0000

    enable openssh server
    Change-Id: I98d37f5428d826eac71162c5409613fee2cd2551
    Signed-off-by: harigollamudi <harigopal.gollamudi@linaro.org>

--- common.mk
+++ common.mk
@@ -72,7 +72,8 @@ PRODUCT_PACKAGES := \
     idlestat \
     nfacct \
     iontest \
-    ion-unit-tests
+    ion-unit-tests \
+    sshd
 
 #packages we are using for benchmarking
 # d8 replaces v8shell -- we're leaving both packages in here for now so
