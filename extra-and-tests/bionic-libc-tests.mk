1aeab518cfb45e9ce4ad27d0b4d5dbe0bdd8663c
Yongqin Liu <yongqin.liu@linaro.org>
2014-10-16 22:17:42 +0800

    bionic test: update according to change on upstream for bionic libc test
    some test are removed on upstream,
    and here is a response for the change.
    
    Change-Id: Ibdacde6091598dc5753d710b765a2dc3e87d5959
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- extra-and-tests/bionic-libc-tests.mk
+++ extra-and-tests/bionic-libc-tests.mk
@@ -9,37 +9,12 @@
 #     Linaro <linaro-android@lists.linaro.org>
 #############################################################################
 
-# pick up tests to build the system image
-#BIONIC_LIBC_NATIVE_TEST_NOT_EXIST := \
-#    bench_stdio \
-#    test_drand48 \
-#    test_pthread_getcpuclockid \
-#    test_strftime_2039 \
-#    test_strptime \
-#    test_tm_zone \
-#    test_libgen \
-#    bench_locks \
-#    test_system \
-#    test_timer_create \
-#    test_timer_create2 \
-#    test_timer_create3 \
-#    test_clone \
-#    bench_pthread_shared \
-#    bench_pthread_static \
-#    test_atomics \
-
-#BIONIC_LIBC_NATIVE_TEST_NOT_USED := \
-#    bench_pthread \
-#    test_thread_max
-
 BIONIC_LIBC_NATIVE_TEST := \
     libdlclosetest1 \
     libdlclosetest2 \
     libtest_relocs \
     libtest_static_init \
     test_aligned \
-    test_clock \
-    test_cpu_set \
     test_dlclose_destruction \
     test_dlopen_null \
     test_executable_destructor \
@@ -49,16 +24,11 @@ BIONIC_LIBC_NATIVE_TEST := \
     test_gethostname \
     test_mutex \
     test_netinet_icmp \
-    test_pthread_cleanup_push \
     test_pthread_cond \
-    test_pthread_create \
-    test_pthread_join \
     test_pthread_mutex \
     test_pthread_once \
     test_pthread_rwlock \
     test_relocs \
-    test_semaphore \
-    test_sem_post \
     test_setjmp \
     test_seteuid \
     test_static_cpp_mutex \
--- extra-and-tests/run-bionic-tests.sh
+++ extra-and-tests/run-bionic-tests.sh
@@ -14,10 +14,7 @@
 #     Test sources are available at "system/extras/tests/bionic/libc"
 #############################################################################
 
-TESTS="test_clock test_cpu_set test_dlclose_destruction test_dlopen_null test_executable_destructor test_getaddrinfo test_getgrouplist test_gethostbyname test_gethostname test_mutex test_netinet_icmp test_pthread_cleanup_push test_pthread_cond test_pthread_create test_pthread_join test_pthread_mutex test_pthread_once test_pthread_rwlock test_relocs test_semaphore test_sem_post test_setjmp test_seteuid test_static_cpp_mutex test_static_executable_destructor test_static_init test_sysconf test_udp"
-
-# Rogue tests
-#TESTS+= "test_cond test_thread_max test_timer_create test_timer_create2 bench_stdio test_drand48 test_pthread_getcpuclockid test_strftime_2039 test_strptime test_tm_zone test_libgen bench_locks test_system test_timer_create3 test_clone bench_pthread_shared bench_pthread_static bench_pthread test_atomics "
+TESTS="test_dlclose_destruction test_dlopen_null test_executable_destructor test_getaddrinfo test_getgrouplist test_gethostbyname test_gethostname test_mutex test_netinet_icmp  test_pthread_cond test_pthread_mutex test_pthread_once test_pthread_rwlock test_relocs test_setjmp test_seteuid test_static_cpp_mutex test_static_executable_destructor test_static_init test_sysconf test_udp"
 
 for TEST in $TESTS; do
 	$TEST
@@ -28,11 +25,3 @@ for TEST in $TESTS; do
 		echo "$TEST : PASS"
 	fi
 done
-
-# test_system date
-# EXIT_STATUS=$?
-#if [ $EXIT_STATUS -ne 0 ]; then
-#	echo "$TEST : FAIL"
-#else
-#	echo "$TEST : PASS"
-#fi
